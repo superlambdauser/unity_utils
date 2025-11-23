fe333e2ae74b369fdfa554db479b8b703bb77940
deepthroadocus <sockpuppetmastaz@proton.me>
2025-11-23 14:51:28 +0100

    Abstract Singletons adapted for Unity projects needs

---
+++ Singletons/PersistentSingleton.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class PersistentSingleton<T> : Singleton<T> where T : MonoBehaviour
+{
+    protected override void Awake()
+    {
+        base.Awake();
+        DontDestroyOnLoad(gameObject); // Persists when changing scenes
+    }
+}
